6e9917cfd22205c12cfddd8faf6d3d2d0a79d9e9
NguyenHuyHoang <huyhoang.77.got@gmail.com>
2023-03-12 23:06:27 +0700

    12/3/2023 11:05 Create project

---
+++ finedining/app/src/main/java/sp23cp18103/nhom2/finedining/LoginActivity.java
@@ -0,0 +1,14 @@
+package sp23cp18103.nhom2.finedining;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
