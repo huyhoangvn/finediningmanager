2a59c70b62643f4dd6648e465070597c6618cf5e
admin <dinhvanvu10a1@gmail.com>
2023-03-15 13:08:36 +0700

    15/3 13:07 thiết kế giao diện hóa đơn,Thống kê doanh thu,DatBanDao,DatMonDao,HoaDonDAO

--- finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/DatBanDAO.java
+++ finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/DatBanDAO.java
@@ -1,4 +1,58 @@
 package sp23cp18103.nhom2.finedining.database;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sp23cp18103.nhom2.finedining.model.DatBan;
+
 public class DatBanDAO {
+    private SQLiteDatabase db;
+
+    public DatBanDAO(Context context){
+        DBHelper dbHelper = new DBHelper(context);
+        db = dbHelper.getWritableDatabase();
+    }
+    public long insertDatBan(DatBan dBan){
+        ContentValues  values = new ContentValues();
+        values.put("maBan",dBan.getMaBan());
+        values.put("maHD",dBan.getMaHD());
+        values.put("thoiGianXuat",dBan.getThoiGianDat());
+
+        return db.insert("datban",null,values);
+    }
+    public int updateDatBan(DatBan dBan){
+        ContentValues  values = new ContentValues();
+        values.put("maBan",dBan.getMaBan());
+        values.put("maHD",dBan.getMaHD());
+        values.put("thoiGianXuat",dBan.getThoiGianDat());
+
+        return db.update("datban",values,"maBan=?",new String[]{String.valueOf(dBan.getMaBan())});
+    }
+
+    public List<DatBan> getAllDatBan(){
+        String sql = "SELECT *FROM datban";
+        return getData(sql);
+    }
+    @SuppressLint("Range")
+    public List<DatBan> getData(String sql, String...SelectArgs){
+        List<DatBan> list = new ArrayList<>();
+        Cursor c = db.rawQuery(sql,SelectArgs);
+        while (c.moveToNext()){
+            DatBan dBan = new DatBan();
+            dBan.setMaBan(c.getInt(c.getColumnIndex("maBan")));
+            dBan.setMaHD(c.getInt(c.getColumnIndex("maHD")));
+            dBan.setThoiGianDat(c.getString(c.getColumnIndex("thoiGianXuat")));
+
+            list.add(dBan);
+        }
+        return list;
+
+
+    }
 }
--- finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/HoaDonDAO.java
+++ finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/HoaDonDAO.java
@@ -1,4 +1,86 @@
 package sp23cp18103.nhom2.finedining.database;
 
-public class HoaDonDAO {
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sp23cp18103.nhom2.finedining.model.HoaDon;
+
+public class HoaDonDAO{
+    private SQLiteDatabase db;
+
+    public HoaDonDAO(Context context){
+        DBHelper dbHelper = new DBHelper(context);
+        db = dbHelper.getWritableDatabase();
+
+
+    }
+    /*insert hóa đơn*/
+    public long insertHoaDon(HoaDon hd){
+        ContentValues values = new ContentValues();
+        values.put("maKH",hd.getMaKH());
+        values.put("maNV",hd.getMaNV());
+        values.put("soLuongKhach",hd.getSoLuongKhach());
+        values.put("thoiGianXuat",hd.getThoiGianXuat());
+        values.put("trangThai",hd.getTrangThai());
+
+        return db.insert("hoadon",null,values);
+
+    }
+    /*update hóa đơn*/
+    public int updateHoaDon(HoaDon hd){
+        ContentValues values = new ContentValues();
+        values.put("maKH",hd.getMaKH());
+        values.put("maNV",hd.getMaNV());
+        values.put("soLuongKhach",hd.getSoLuongKhach());
+        values.put("thoiGianXuat",hd.getThoiGianXuat());
+        values.put("trangThai",hd.getTrangThai());
+
+        return db.update("hoadon",null,"maHD=?",new String[]{String.valueOf(hd.getMaHD())});
+
+    }
+    /*getAll danh sach hóa đơn*/
+    public List<HoaDon> getAllHoaDon(){
+        String sql ="SELECT *FROM hoadon";
+        return getData(sql);
+    }
+    /* hóa đơn*/
+    public HoaDon getID(String id){
+        String sql = "SELECT *FROM hoadon WHERE maHD=?";
+        List<HoaDon> list = getData(sql,id);
+        if(list==null){
+            return null;
+        }else
+            return list.get(0);
+    }
+    @SuppressLint("Range")
+    public List<HoaDon> getData(String sql, String...SelectArgs){
+        List<HoaDon> list = new ArrayList<>();
+        Cursor c = db.rawQuery(sql,SelectArgs);
+        while (c.moveToNext()){
+            HoaDon hd = new HoaDon();
+            hd.setMaHD(c.getInt(c.getColumnIndex("maHD")));
+            hd.setMaKH(c.getInt(c.getColumnIndex("maKH")));
+            hd.setMaNV(c.getInt(c.getColumnIndex("maNV")));
+            hd.setSoLuongKhach(c.getInt(c.getColumnIndex("soLuongKhach")));
+            hd.setThoiGianXuat(c.getString(c.getColumnIndex("thoiGianXuat")));
+            hd.setTrangThai(c.getInt(c.getColumnIndex("trangThai")));
+
+            list.add(hd);
+
+        }
+        return list;
+    }
+
+/*
+*
+* */
+
+
+
 }
