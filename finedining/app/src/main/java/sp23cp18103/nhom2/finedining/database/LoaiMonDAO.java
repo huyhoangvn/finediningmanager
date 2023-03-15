db9773080c35020e970b1d5aa9d5e591244e21d2
thuan <kingofwar2062003@gmail.com>
2023-03-15 15:38:12 +0700

    ngay 15/3 3:26 thiet ke giao dien loai mon, mon, tao , tao loaimonDAO, monDAO

--- finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/LoaiMonDAO.java
+++ finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/LoaiMonDAO.java
@@ -1,4 +1,53 @@
 package sp23cp18103.nhom2.finedining.database;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sp23cp18103.nhom2.finedining.model.HoaDon;
+import sp23cp18103.nhom2.finedining.model.LoaiMon;
+
 public class LoaiMonDAO {
+    SQLiteDatabase db;
+    public LoaiMonDAO(Context context){
+        DBHelper helper = new DBHelper(context);
+        db = helper.getWritableDatabase();
+    }
+    public long insertLoaiMon(LoaiMon lm){
+        ContentValues values = new ContentValues();
+        values.put("maNV",lm.getMaNV());
+        values.put("tenLoai", lm.getTenLoai());
+        values.put("trangThai", lm.getTrangThai());
+        return db.insert("loaimon", null, values);
+    }
+    public int updateLoaiMon(LoaiMon lm){
+        ContentValues values = new ContentValues();
+        values.put("maNV",lm.getMaNV());
+        values.put("tenLoai", lm.getTenLoai());
+        values.put("trangThai", lm.getTrangThai());
+        return db.update("loaimon", values,"maLM = ?", new String[]{String.valueOf(lm.getMaLM())});
+    }
+    public List<LoaiMon> getAllLoaiMon(){
+        String sql ="SELECT *FROM loaimon";
+        return getData(sql);
+    }
+    @SuppressLint("Range")
+    public List<LoaiMon> getData(String sql, String...SelectArgs){
+        List<LoaiMon> list = new ArrayList<>();
+        Cursor c = db.rawQuery(sql,SelectArgs);
+        while (c.moveToNext()){
+            LoaiMon lm = new LoaiMon();
+            lm.setMaLM(c.getInt(c.getColumnIndex("maLM")));
+            lm.setMaNV(c.getInt(c.getColumnIndex("maNV")));
+            lm.setTenLoai(c.getString(c.getColumnIndex("tenLoai")));
+            lm.setTrangThai(c.getInt(c.getColumnIndex("trangThai")));
+            list.add(lm);
+        }
+        return list;
+    }
 }
