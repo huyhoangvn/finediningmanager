aab71994c8993237bdf899a72deda565e99f42a5
NguyenHuyHoang <huyhoang.77.got@gmail.com>
2023-03-16 20:57:11 +0700

    16/3/2023 9:00 NhaHangDAO, NhanVienDAO utils Shared Preferences

--- finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/NhanVienDAO.java
+++ finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/NhanVienDAO.java
@@ -1,4 +1,36 @@
 package sp23cp18103.nhom2.finedining.database;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import sp23cp18103.nhom2.finedining.model.NhaHang;
+import sp23cp18103.nhom2.finedining.model.NhanVien;
+
+/*
+ * Tạo bảng nhân viên
+ * nhanvien ( maNV, maNH, tenNV, gioiTinh, ngaySinh, sdt, phanQuyen, trangThai, hinh, taiKhoan, matKhau )
+ * */
 public class NhanVienDAO {
+    private SQLiteDatabase db;
+
+    public NhanVienDAO(Context context){
+        DBHelper dbHelper = new DBHelper(context);
+        db = dbHelper.getWritableDatabase();
+    }
+
+    public long insertNhaHang(NhanVien nhanVien){
+        ContentValues values = new ContentValues();
+        values.put("maNV",nhanVien.getMaNV());
+        values.put("maNH",nhanVien.getMaNH());
+        values.put("tenNV", nhanVien.getTenNV());
+        values.put("gioiTinh", nhanVien.getGioiTinh());
+        values.put("ngaySinh", nhanVien.getNgaySinh());
+        values.put("sdt", nhanVien.getSdt());
+        values.put("trangThai", nhanVien.getTrangThai());
+        values.put("taiKhoan", nhanVien.getTaiKhoan());
+        values.put("matKhau", nhanVien.getMatKhau());
+        values.put("hinh", nhanVien.getHinh());
+        return db.insert("nhanvien",null,values);
+    }
 }
