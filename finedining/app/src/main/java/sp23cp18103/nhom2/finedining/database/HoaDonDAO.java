ddbcf06a265cfcc08ac39fcc50d40edffb106edc
NguyenHuyHoang <huyhoang.77.got@gmail.com>
2023-03-28 21:54:51 +0700

    28/3/2023 Sửa lỗi Hóa đơn

--- finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/HoaDonDAO.java
+++ finedining/app/src/main/java/sp23cp18103/nhom2/finedining/database/HoaDonDAO.java
@@ -26,8 +26,7 @@ public class HoaDonDAO{
         values.put("maNV",hd.getMaNV());
         values.put("soLuongKhach",hd.getSoLuongKhach());
         values.put("thoiGianXuat",hd.getThoiGianXuat());
-        values.put("thoiGianĐat",hd.getThoiGianĐat());
-        values.put("thoiGianThanhToan",hd.getThoiGianThanhToan());
+        values.put("thoiGianDat",hd.getThoiGianDat());
         values.put("trangThai",hd.getTrangThai());
 
         return db.insert("hoadon",null,values);
@@ -40,8 +39,7 @@ public class HoaDonDAO{
         values.put("maNV",hd.getMaNV());
         values.put("soLuongKhach",hd.getSoLuongKhach());
         values.put("thoiGianXuat",hd.getThoiGianXuat());
-        values.put("thoiGianĐat",hd.getThoiGianĐat());
-        values.put("thoiGianThanhToan",hd.getThoiGianThanhToan());
+        values.put("thoiGianDat",hd.getThoiGianDat());
         values.put("trangThai",hd.getTrangThai());
 
         return db.update("hoadon",values,"maHD=?",new String[]{String.valueOf(hd.getMaHD())});
@@ -101,8 +99,7 @@ public class HoaDonDAO{
             hd.setMaNV(c.getInt(c.getColumnIndex("maNV")));
             hd.setSoLuongKhach(c.getInt(c.getColumnIndex("soLuongKhach")));
             hd.setThoiGianXuat(c.getString(c.getColumnIndex("thoiGianXuat")));
-            hd.setThoiGianĐat(c.getString(c.getColumnIndex("thoiGianĐat")));
-            hd.setThoiGianThanhToan(c.getString(c.getColumnIndex("thoiGianThanhToan")));
+            hd.setThoiGianDat(c.getString(c.getColumnIndex("thoiGianDat")));
             hd.setTrangThai(c.getInt(c.getColumnIndex("trangThai")));
             list.add(hd);
         }
--- finedining/app/src/main/java/sp23cp18103/nhom2/finedining/model/HoaDon.java
+++ finedining/app/src/main/java/sp23cp18103/nhom2/finedining/model/HoaDon.java
@@ -8,18 +8,16 @@ public class HoaDon {
     private int maNV;
     private int soLuongKhach;
     private String thoiGianXuat;//YYYY-mm-dd hh:mm
-    private String thoiGianĐat;//YYYY-mm-dd hh:mm
-    private String thoiGianThanhToan;//YYYY-mm-dd hh:mm
+    private String thoiGianDat;//YYYY-mm-dd hh:mm
     private int trangThai;// 1:"Đang đặt"; 2:"Chờ thanh toán"; 3:"Đã thanh toán"; 0:"Hủy"
 
-    public HoaDon(int maHD, int maKH, int maNV, int soLuongKhach, String thoiGianXuat, String thoiGianĐat, String thoiGianThanhToan, int trangThai) {
+    public HoaDon(int maHD, int maKH, int maNV, int soLuongKhach, String thoiGianXuat, String thoiGianThanhToan, int trangThai) {
         this.maHD = maHD;
         this.maKH = maKH;
         this.maNV = maNV;
         this.soLuongKhach = soLuongKhach;
         this.thoiGianXuat = thoiGianXuat;
-        this.thoiGianĐat = thoiGianĐat;
-        this.thoiGianThanhToan = thoiGianThanhToan;
+        this.thoiGianDat = thoiGianDat;
         this.trangThai = trangThai;
     }
 
@@ -74,20 +72,12 @@ public class HoaDon {
         this.trangThai = trangThai;
     }
 
-    public String getThoiGianĐat() {
-        return thoiGianĐat;
+    public String getThoiGianDat() {
+        return thoiGianDat;
     }
 
-    public void setThoiGianĐat(String thoiGianĐat) {
-        this.thoiGianĐat = thoiGianĐat;
-    }
-
-    public String getThoiGianThanhToan() {
-        return thoiGianThanhToan;
-    }
-
-    public void setThoiGianThanhToan(String thoiGianThanhToan) {
-        this.thoiGianThanhToan = thoiGianThanhToan;
+    public void setThoiGianDat(String thoiGianĐat) {
+        this.thoiGianDat = thoiGianDat;
     }
 
     /*
